69a16965d3ce3fc4bd02635bf4e712686623939e
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-04 00:14:32 -0600

    Added FunRestController.java

---
+++ src/main/java/com/henry/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.henry.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() { return "Hello World"; }
+}
