ef6200c73a808dc773ab5a1205b8b731d9dccfe8
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-06 00:13:08 -0600

    Added FunRestController.java

--- src/main/java/com/henry/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/henry/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -8,5 +8,19 @@ public class FunRestController {
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")
-    public String sayHello() { return "Hello World"; }
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
 }
