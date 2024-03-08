d2bb7428f75ef4ba7454dd32e6dfc5ce0a779388
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-08 15:33:24 -0600

    Modified: Added context path, changed server port.

--- src/main/java/com/henry/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/henry/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.henry.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "team info"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")
